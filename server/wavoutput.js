00c70c8aca35f8ab795a16116f646eeb12affd05
Matt Brennan <mattyb1000@gmail.com>
2012-05-01 03:36:35 +0100

    send the right length

--- server/wavoutput.js
+++ server/wavoutput.js
@@ -10,7 +10,7 @@
     function u16(i){
       return [i & 0xFF, i >> 8 & 0xFF];
     }
-    CHUNK_SIZE = 400;
+    CHUNK_SIZE = 800;
     prototype.readable = true;
     function WavOutput(length, rate, channels, bytes){
       var _this = this;
@@ -19,9 +19,9 @@
       this.channels = channels != null ? channels : 2;
       this.bytes = bytes != null ? bytes : 2;
       this.samples = this.length * this.rate;
-      this.byteLength = this.bytes * this.channels * this.samples;
       this.runningBuf = new Buffer(this.bytes * this.channels * CHUNK_SIZE);
       this.pad = this.bytes * this.channels * this.samples % 2;
+      this.byteLength = 4 + (8 + 16) + (8 + this.bytes * this.samples + this.pad);
       this.head = new Buffer([].concat(0x52, 0x49, 0x46, 0x46, u32(4 + (8 + 16) + (8 + this.bytes * this.samples + this.pad)), 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, u32(16), u16(1), u16(this.channels), u32(this.rate), u32(this.rate * this.bytes * this.channels), u16(this.bytes * this.channels), u16(8 * Math.ceil(this.bytes)), 0x64, 0x61, 0x74, 0x61, u32(this.bytes * this.channels * this.samples)));
       process.nextTick(function(){
         return _this.emit('data', _this.head);
@@ -50,7 +50,7 @@
         }
         _this.runningBuf["writeUInt" + _this.bytes * 8 + "LE"](it, _this.off);
         _this.off += _this.bytes;
-        if (_this.cursor === _this.samples) {
+        if (_this.cursor === _this.samples * _this.channels) {
           _this.emit('end');
         }
         if (!_this.paused) {
