fe13ffff3c366739e2398c2abc451555186e640e
Matt Brennan <mattyb1000@gmail.com>
2012-05-01 03:33:53 +0100

    be consistent about chunk_size, use cluster

--- server/main.js
+++ server/main.js
@@ -1,37 +1,46 @@
 (function(){
-  var Serve, Static, Redirect, Template, Reader, WavOutput, Coco, conf, app, _ref;
+  var Serve, Static, Redirect, Template, Reader, WavOutput, Coco, cluster, os, conf, cpu, app, _ref, _i, _len;
   _ref = require('sabor'), Serve = _ref.Serve, Static = _ref.Static, Redirect = _ref.Redirect, Template = _ref.Template;
   Reader = require('q-io').Reader;
   WavOutput = require("./wavoutput").WavOutput;
   Coco = require('coco');
+  cluster = require('cluster');
+  os = require('os');
   conf = __import({
     port: 8002
   }, Coco.run("return " + process.argv.slice(2).join(" ") || "{}", {
     bare: true
   }));
-  app = Serve({
-    "/": function(req){
-      return Template("app/home.eco").render();
-    },
-    "/sound": function(){
-      var wav;
-      wav = new WavOutput;
-      return {
-        body: Reader(wav),
-        status: 200,
-        headers: {
-          'content-type': "audio/wave",
-          'content-length': wav.byteLength
-        }
-      };
-    },
-    "/favicon.ico": function(){
-      return Redirect("client/favicon.png");
-    },
-    "/static/(.*)": Static.dir("client")
-  });
-  app.listen(conf.port, conf.host);
-  console.log("Listening on " + (conf.host || '*') + ":" + conf.port);
+  if (cluster.isMaster) {
+    for (_i = 0, _len = (_ref = os.cpus()).length; _i < _len; ++_i) {
+      cpu = _ref[_i];
+      cluster.fork();
+    }
+  } else {
+    app = Serve({
+      "/": function(req){
+        return Template("app/home.eco").render();
+      },
+      "/sound": function(){
+        var wav;
+        wav = new WavOutput;
+        return {
+          body: Reader(wav),
+          status: 200,
+          headers: {
+            'content-type': "audio/wave",
+            'content-length': wav.byteLength
+          }
+        };
+      },
+      "/favicon.ico": function(){
+        return Redirect("client/favicon.png");
+      },
+      "/static/(.*)": Static.dir("client")
+    });
+    app.listen(conf.port, conf.host);
+    console.log(process.pid + " listening on " + (conf.host || '*') + ":" + conf.port);
+  }
   function __import(obj, src){
     var own = {}.hasOwnProperty;
     for (var key in src) if (own.call(src, key)) obj[key] = src[key];
--- server/wavoutput.js
+++ server/wavoutput.js
@@ -3,14 +3,14 @@
   Stream = require('stream');
   exports.WavOutput = WavOutput = (function(superclass){
     WavOutput.displayName = 'WavOutput';
-    var WAVE_FORMAT_PCM, prototype = __extend(WavOutput, superclass).prototype, constructor = WavOutput;
+    var CHUNK_SIZE, prototype = __extend(WavOutput, superclass).prototype, constructor = WavOutput;
     function u32(i){
       return [i & 0xFF, i >> 8 & 0xFF, i >> 16 & 0xFF, i >> 24 & 0xFF];
     }
     function u16(i){
       return [i & 0xFF, i >> 8 & 0xFF];
     }
-    WAVE_FORMAT_PCM = new Buffer('0001', 'hex');
+    CHUNK_SIZE = 400;
     prototype.readable = true;
     function WavOutput(length, rate, channels, bytes){
       var _this = this;
@@ -20,7 +20,7 @@
       this.bytes = bytes != null ? bytes : 2;
       this.samples = this.length * this.rate;
       this.byteLength = this.bytes * this.channels * this.samples;
-      this.runningBuf = new Buffer(this.bytes * this.channels * 100);
+      this.runningBuf = new Buffer(this.bytes * this.channels * CHUNK_SIZE);
       this.pad = this.bytes * this.channels * this.samples % 2;
       this.head = new Buffer([].concat(0x52, 0x49, 0x46, 0x46, u32(4 + (8 + 16) + (8 + this.bytes * this.samples + this.pad)), 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, u32(16), u16(1), u16(this.channels), u32(this.rate), u32(this.rate * this.bytes * this.channels), u16(this.bytes * this.channels), u16(8 * Math.ceil(this.bytes)), 0x64, 0x61, 0x74, 0x61, u32(this.bytes * this.channels * this.samples)));
       process.nextTick(function(){
@@ -43,14 +43,14 @@
     prototype.tick = function(){
       var _this = this;
       return this.generate(this.cursor++, function(it){
-        if (_this.off >= _this.bytes * _this.channels * 100) {
+        if (_this.off >= _this.bytes * _this.channels * CHUNK_SIZE) {
           _this.emit('data', _this.runningBuf);
-          _this.runningBuf = new Buffer(_this.bytes * _this.channels * 100);
+          _this.runningBuf = new Buffer(_this.bytes * _this.channels * CHUNK_SIZE);
           _this.off = 0;
         }
         _this.runningBuf["writeUInt" + _this.bytes * 8 + "LE"](it, _this.off);
         _this.off += _this.bytes;
-        if (_this.cursor === _this.samples * _this.channels) {
+        if (_this.cursor === _this.samples) {
           _this.emit('end');
         }
         if (!_this.paused) {
