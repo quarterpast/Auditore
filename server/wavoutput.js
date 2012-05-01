892a4fea2ac378eedab84bd88daa04bdadcb446d
Matt Brennan <mattyb1000@gmail.com>
2012-05-01 02:50:59 +0100

    q-io doesn't resume the stream, grumble

--- server/wavoutput.js
+++ server/wavoutput.js
@@ -13,6 +13,7 @@
     WAVE_FORMAT_PCM = new Buffer('0001', 'hex');
     prototype.readable = true;
     function WavOutput(length, rate, channels, bytes){
+      var _this = this;
       this.length = length != null ? length : 30;
       this.rate = rate != null ? rate : 44100;
       this.channels = channels != null ? channels : 2;
@@ -21,20 +22,20 @@
       this.runningBuf = new Buffer(this.bytes * this.channels * 100);
       this.pad = this.bytes * this.channels * this.samples % 2;
       this.head = new Buffer([].concat(0x52, 0x49, 0x46, 0x46, u32(4 + (8 + 16) + (8 + this.bytes * this.samples + this.pad)), 0x57, 0x41, 0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, u32(16), u16(1), u16(this.channels), u32(this.rate), u32(this.rate * this.bytes * this.channels), u16(this.bytes * this.channels), u16(8 * Math.ceil(this.bytes)), 0x64, 0x61, 0x74, 0x61, u32(this.bytes * this.channels * this.samples)));
+      process.nextTick(function(){
+        return _this.emit('data', _this.head);
+      });
+      process.nextTick(__bind(this, 'tick'));
     }
     prototype.headless = true;
     prototype.readable = true;
-    prototype.paused = true;
+    prototype.paused = false;
     prototype.cursor = 0;
     prototype.off = 0;
     prototype.pause = function(){
       return this.paused = true;
     };
     prototype.resume = function(){
-      if (this.headless) {
-        this.emit('data', this.head);
-        this.headless = false;
-      }
       this.paused = false;
       return process.nextTick(__bind(this, 'tick'));
     };
